7456c229e78bfa62db92ce7a3bff30211a088ee5
irfan <myoratech@gmail.com>
2025-09-25 11:24:22 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+// _abc/app/page.js
+export default function Page() {
+  return (
+    <main style={{minHeight:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>
+      <h1>Hi — Next.js in _abc (deployed to Vercel)</h1>
+    </main>
+  )
+}
